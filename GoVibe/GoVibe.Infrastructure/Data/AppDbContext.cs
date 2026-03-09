c74e54afc2bdc796d1f2200214acb7bbf3125913
VinhTin-AQUA <tinhovinh@gmail.com>
2026-03-09 22:10:03 +0700

    Apply .gitignore

--- GoVibe/GoVibe.Infrastructure/Data/AppDbContext.cs
+++ GoVibe/GoVibe.Infrastructure/Data/AppDbContext.cs
@@ -16,5 +16,27 @@ namespace GoVibe.Infrastructure.Data
         public DbSet<PlaceImage> PlaceImages { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<ReviewImage> ReviewImages { get; set; }
+        
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries<Entity>();
+            var now = DateTime.UtcNow;
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = now;
+                    entry.Entity.UpdatedAt = now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAt = now;
+                    entry.Property(x => x.CreatedAt).IsModified = false;
+                }
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
